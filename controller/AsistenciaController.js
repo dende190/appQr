81d8cee72801bb5ddeff9d5d3b6396599bf7e4e1
root <root@localhost.localdomain>
2019-02-11 12:24:22 -0500

    codigo qr implementado estaticamente

--- controller/AsistenciaController.js
+++ controller/AsistenciaController.js
@@ -3,6 +3,19 @@ var router = express.Router();
 
 module.exports = {
 	dash: function(req, res){
-		res.render('viewQr.ejs')
+		var QRCode = require('qrcode')
+ 
+		QRCode.toDataURL(`http://172.16.10.13:3000/sockets?n=2&a=1`,{
+			scale:10,
+		})
+		.then(url => {
+		  res.render('viewQr.ejs',{
+			'imagensita':url
+		})
+		})
+		.catch(err => {
+		  console.error(err)
+		})
+
 	}
 }
