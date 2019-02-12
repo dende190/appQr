0c17bfcaa385dfe51ce0ad9473c088818abd03db
cristian cucunuba <cristiancamilo.cucunuba@gmail.com>
2019-02-12 11:03:55 -0500

    Organizacion de Querys

--- controller/CalendarioController.js
+++ controller/CalendarioController.js
@@ -3,8 +3,8 @@ const mysql = require("../config/database")
 const router = express.Router();
 var crypto = require('crypto');
 var assert = require('assert');
+const {calendarioClases} = require('../utils/sql')
 
-const query = "SELECT clase.id AS id_clase, profesor.nombres AS nombre_profesor, profesor.apellidos AS apellido_profesor, modalidad.nombre AS modalidad, nivel.nombre AS nivel, salon.nombre AS salon, clase.hora_inicio, clase.hora_final, estudiante.nombres AS nombre_estudiante, estudiante.apellidos AS apellido_estudiante, estudiante.id AS id_estudiante, salon.id AS id_salon FROM clase INNER JOIN salon ON salon.id = clase.id_salon INNER JOIN curso ON curso.id = clase.id_curso INNER JOIN nivel ON nivel.id = curso.id_nivel INNER JOIN modalidad ON modalidad.id = curso.id_modalidad INNER JOIN clase_estudiante ON clase_estudiante.id_clase = clase.id INNER JOIN estudiante ON estudiante.id = clase_estudiante.id_estudiante INNER JOIN clase_profesor ON clase_profesor.id_clase = clase.id INNER JOIN profesor ON profesor.id = clase_profesor.id_profesor WHERE clase.fecha_final > '2019-02-12 00:00:00'"
 
 module.exports = {
     dash: function (req, res) {
@@ -35,7 +35,7 @@ module.exports = {
         const dataBase = await mysql.conexionApp()
         if (dataBase) {
             try {
-                let [rows, fields] = await dataBase.query(query);
+                let [rows, fields] = await dataBase.query(calendarioClases);
                 result = rows;
             } catch (e) {
                 console.log("(ERROR)" + e);
