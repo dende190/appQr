a045d7616fe10387efa3e8d4faef1cf2ce7cc96c
SneiderSanchez <sneiderandres09@hotmail.com>
2019-02-13 11:57:34 -0500

    Paseo por la barberia del qr y cositas varias

--- controller/AsistenciaController.js
+++ controller/AsistenciaController.js
@@ -5,8 +5,8 @@ module.exports = {
 	dash: function(req, res){
 		var QRCode = require('qrcode')
  
-		QRCode.toDataURL(`http://172.16.10.12:3000/sockets?e=17&s=5&c=45&p=8`,{
-			scale:7,
+		QRCode.toDataURL(`http://172.16.10.14:3000/sockets?e=17&s=5&c=45&p=8`,{
+			scale:9,
 			
 		})
 		.then(url => {
